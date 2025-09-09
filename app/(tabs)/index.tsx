f88d7758ce5c279dd2d7542a2e6819d468d688a8
Emily <emily.madsen03@gmail.com>
2025-09-09 13:19:39 -0700

    added base login screen, waiting for auth.ts to finish (#22)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,10 @@
 import { Link } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Redirect } from "expo-router";
 export default function Index() {
   return (
+
+
     <View style={styles.container}>
       <Text style={styles.text}>Home screen</Text>
       <Link href="/about" style={styles.button}>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,45 @@
-import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import Login from './(auth)/login';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+
+    <View style={styles.container}>
+      <Login/>
+      <StatusBar style="auto" />
+    </View>
+    // <Stack>
+    //   <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    // </Stack>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
+
+
+// // app/login.tsx
+// import { Text, View } from "react-native";
+
+// export default function Login() {
+//   return (
+//     <View style={{ padding: 24 }}>
+//       <Text>Login screen</Text>
+//     </View>
+//   );
+// }
